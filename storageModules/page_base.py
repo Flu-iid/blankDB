9bea4826ae897a24bc4ef3a79e4d95883601d7c3
flu <57827666+Flu-iid@users.noreply.github.com>
2025-05-16 23:14:20 +0330

    reorganized

--- storageModules/page_base.py
+++
@@ -1,9 +0,0 @@
-class Engine:
-    PAGE_SIZE = 4096
-
-    def __init__(self):
-        self.buffer = bytearray(self.PAGE_SIZE)
-        self.record_offsets = []
-
-    def add_record(self, record_data):
-        pass
