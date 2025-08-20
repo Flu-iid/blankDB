246acb5d6700b558009f8abba664c9bc48ed60e2
flu <57827666+Flu-iid@users.noreply.github.com>
2025-08-20 07:04:37 +0330

    changed input repr and refigured output return values

--- src/handler/default/handler.py
+++ src/handler/default/handler.py
@@ -12,6 +12,8 @@ if TYPE_CHECKING:
 class Handler:
     """handle user commands as an object"""
 
+    blankDB_repr = ["␢_db", "_␢", "␣db", "_DB"]
+
     def __init__(self, analyzer) -> None:
         self.user_input = ""
         self.exit_signal = False
@@ -22,15 +24,25 @@ class Handler:
         """i: input value, e: exit signal value"""
         return f"Handler(i:{self.user_input})[e:{self.exit_signal}]"
 
-    def get_input(self):
-        """get user input"""
-        self.user_input = input("_DB > ")
+    def get_input(self, prompt_repr: int = 0):
+        """get user input.\ncan use prompt representation between:
+        1: ␢_db
+        2: _␢
+        3: ␣db
+        4: _DB
+        """
+
+        self.user_input = input(f"{Handler.blankDB_repr[prompt_repr]} > ")
         if self.user_input in ["!!", "exit"]:
             self.exit_signal = True
 
     def get_output(self):
-        self.output = self.analyzer.analyze(self.user_input)
-        return self.output
+        if not self.exit_signal:
+            self.output = self.analyzer.analyze(self.user_input)
+            return self.output
+        else:
+            return False, None
+
         # display result from view
 
     def logout(self):
