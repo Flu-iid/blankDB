d103a1b07cfe1a3cf87695dd3aa1a257981e8eef
flu <57827666+Flu-iid@users.noreply.github.com>
2025-08-31 20:52:25 +0330

    incudes precedence parser

--- src/typing.py
+++ src/typing.py
@@ -1,6 +1,6 @@
 from src.handler.default.handler import Handler
 from src.parser.default.analyzer import Analyzer
-from src.parser.default.tokenizer import Tokenizer
-# from src.parser.default.parser import Parser
+from src.parser.default.tokenizer import Tokenizer, Token, Tkeyword, Tid, Tint
+# from src.parser.default.precedence_parser import Pair
 # from src.engine.default.engine import Engine
 # from src.view.default.view import Handler
