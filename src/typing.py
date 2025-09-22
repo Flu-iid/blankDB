758a2d370589108783a1e232f3342066749669f6
flu <57827666+Flu-iid@users.noreply.github.com>
2025-09-23 02:56:26 +0330

    not needed for now

--- src/typing.py
+++ src/typing.py
@@ -1,7 +1,7 @@
-from src.handler.default.handler import Handler
-from src.parser.default.analyzer import Analyzer
-from src.parser.default.tokenizer import Tid, Tint, Tkeyword, Token, Tokenizer
+# from src.handler.default.handler import Handler
+# from src.parser.default.analyzer import Analyzer
+# from src.parser.default.tokenizer import Tid, Tint, Tkeyword, Token, Tokenizer
 
-# from src.parser.default.precedence_parser import Pair
-# from src.engine.default.engine import Engine
-# from src.view.default.view import Handler
+# # from src.parser.default.precedence_parser import Pair
+# # from src.engine.default.engine import Engine
+# # from src.view.default.view import Handler
